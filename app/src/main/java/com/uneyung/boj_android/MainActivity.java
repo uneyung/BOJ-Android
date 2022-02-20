d15d37911860164d8e7e33b8b7448f698e0a34e1
uneyung <man8532@naver.com>
2022-02-21 02:48:38 +0900

    edit

--- app/src/main/java/com/uneyung/boj_android/MainActivity.java
+++ app/src/main/java/com/uneyung/boj_android/MainActivity.java
@@ -2,13 +2,9 @@ package com.uneyung.boj_android;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,29 +12,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
-        button.setOnClickListener(this::onButton1Clicked);
-    }
 
-    public void onButton1Clicked(View V) {
-        DateFormat day = new SimpleDateFormat("E");
-        DateFormat hour = new SimpleDateFormat("K");
-        DateFormat minute = new SimpleDateFormat("m");
-        Date date = new Date();
-        int problems_count = 0;
-        int solve_way = 1;
+        Calendar cal = Calendar.getInstance();
+        int day = cal.get(Calendar.DAY_OF_WEEK);
+        String text_day = "";
 
-        if (day.format(date).equals("Mon") || day.format(date).equals("Wen") || day.format(date).equals("Fri")) {
-            if (hour.format(date).equals("9") && minute.format(date).equals("0")) {
-                Toast.makeText(this, "오늘은 " +
-                        day.format(date) + "요일로 백준 풀 시간입니다.", Toast.LENGTH_LONG).show();
-            } else if (hour.format(date).equals("18") && minute.format(date).equals("0")) {
-                if (problems_count >= 2)
-                    Toast.makeText(this, "백준 푸는 시간이 끝났습니다. 축하합니다.", Toast.LENGTH_LONG).show();
-                else
-                    Toast.makeText(this, "백준 푸는 시간이 끝났습니다. 선정했던 2문제 중 " + problems_count + "문제를 풀었습니다.", Toast.LENGTH_LONG).show();
-            }
+        if(day == 2 || day == 4 || day == 6){
+            if(day == 2)
+                text_day = "월";
+            else if(day == 4)
+                text_day = "수";
+            else
+                text_day = "금";
 
+            Toast.makeText(this, "오늘은 " +
+                    text_day + "요일로 백준 푸는 날입니다.", Toast.LENGTH_LONG).show();
         }
     }
 }
